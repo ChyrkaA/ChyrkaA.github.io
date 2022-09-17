ce51c8184318e03f83063adf9efda6ca2bbd9a8f
ChyrkaA <andrey.chirka@gmail.com>
2022-09-18 00:08:53 +0300

    agroflex

---
+++ agroflex/js/script.js
@@ -0,0 +1,90 @@
+'use strict';
+window.addEventListener('DOMContentLoaded', () => {        
+//jQery
+
+        $('.distrib__slider').slick({
+                centerMode: true,
+                centerPadding: '50px',
+                slidesToShow: 5,
+                dots: true,
+                variableWidth:true,
+                prevArrow: $('.distrib__prev_btn'),
+                nextArrow: $('.distrib__next_btn'),
+                responsive: [{
+                                breakpoint: 768,
+                                settings: {
+                                        arrows: false,
+                                        centerMode: true,
+                                        centerPadding: '40px',
+                                        slidesToShow: 3
+                                }
+                        },
+                        {
+                                breakpoint: 480,
+                                settings: {
+                                        arrows: false,
+                                        centerMode: true,
+                                        centerPadding: '40px',
+                                        slidesToShow: 1
+                                }
+                        }
+                ]
+        });
+
+//js
+
+        const tabParent = document.querySelector('.catalog__tabs');
+        const tab = document.querySelectorAll('.catalog__tab');
+        const content = document.querySelectorAll('.catalog__content');
+        const tabTitle = document.querySelectorAll('.catalog__tab-title');
+        
+        function hideTabContent() {
+                content.forEach(item => {
+                        item.classList.remove('catalog__content_active');
+                });
+                tab.forEach(item => {
+                        item.classList.remove('catalog__tab_active');
+                });
+        }
+        
+        function showTabContent(i = 0) {
+                content[i].classList.add('catalog__content_active');
+                tab[i].classList.add('catalog__tab_active');
+        }
+        
+        hideTabContent();
+        showTabContent();
+        
+        tabParent.addEventListener('click', (event) => {
+                const target = event.target;
+                if (target && target.classList.contains('catalog__tab-title')) {
+                        tabTitle.forEach((item, i) => {
+                                if (target == item) {
+                                        hideTabContent();
+                                        showTabContent(i);                                        
+                                }
+                        });
+                }
+        });
+
+        const pageUp = document.querySelector('.fa-w-16'); 
+        window.addEventListener('scroll', ()=>{
+                if (window.scrollY > 800){
+                        pageUp.classList.remove('hide');
+                }
+                else{
+                        pageUp.classList.add('hide');
+                }
+        });
+        const arrows = document.querySelector('.promo__arrows'); 
+        window.addEventListener('scroll', ()=>{
+                if (window.scrollY > 200){
+                        arrows.classList.add('hide');
+                }
+                else{
+                        arrows.classList.remove('hide');
+                }
+        });
+       
+});
+
