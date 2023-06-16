1246d54beb6c45fec9a8f8f61b6835466b482fba
ChyrkaA <andrey.chirka@gmail.com>
2023-06-17 00:01:51 +0300

    update

--- pdacademy/js/pages.js
+++ pdacademy/js/pages.js
@@ -1,7 +1,12 @@
 'use strict';
 
-window.addEventListener('DOMContentLoaded', () => {
+import menuBlock from "./modules/menu";
+import pageUp from "./modules/pageUp";
+import {
+    videoHeight
+} from "./services/constFunctions";
 
+window.addEventListener('DOMContentLoaded', () => {
     const preload = document.querySelector('.preload');
 
     function ready() {
@@ -11,68 +16,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     window.addEventListener("load", ready);
 
-    const menuBtn = document.querySelectorAll('.menu__item_link');
-
-    menuBtn.forEach(item => {
-        item.onmousemove = (e) => {
-            item.onclick = () => {
-                let circle = document.createElement('div');
-                circle.classList.add('click');
-                circle.style.left = (e.clientX - item.offsetLeft - 705) + 'px';
-                circle.style.top = (e.clientY - item.offsetTop) + 'px';
-                item.appendChild(circle);
-
-                setTimeout(() => {
-                    circle.remove()
-                }, 1000)
-            }
-        }
-    })
-
-    const pageUp = document.querySelector('.pageup');
-
-    const fadeIn = (el, timeout, opacity, visibility, display) => {
-        el.style.opacity = opacity || 1;
-        el.style.visibility = visibility || 'visible';
-        el.style.display = display || 'block';
-        el.style.transition = `all ${timeout}ms`;
-    };
-
-    const fadeOut = (el, timeout, opacity, visibility) => {
-        el.style.opacity = opacity || 0;
-        el.style.visibility = visibility || 'hidden';
-        el.style.transition = `all ${timeout}ms`;
-    };
-
-    const logo = document.querySelector('.menu__logo');
-    const menuWrapper = document.querySelector('.menu__wrapper');
-
-    window.addEventListener('scroll', () => {
-
-        if (scrollY >= 800) {
-            fadeIn(pageUp, 1000, 0.7);
-        } else {
-            fadeOut(pageUp, 1000, 0);
-        }
-
-        if (scrollY >= 200) {
-            logo.classList.remove('bigLogo');
-            menuWrapper.classList.remove('flex-end');
-        } else {
-            logo.classList.add('bigLogo');
-            menuWrapper.classList.add('flex-end');
-        }
-
-    });
-
-    function videoHeight(classTrigger) {
-        const sectionVideo = document.querySelector(classTrigger);
-        if (sectionVideo) {
-            const videoBlock = sectionVideo.querySelector('iframe');
-            const widthVideoBlock = window.getComputedStyle(videoBlock, null).getPropertyValue('width');
-            const heightVideoBlock = Math.round(+(widthVideoBlock.slice(0, -2)) * 0.5625);
-            sectionVideo.style.height = `${heightVideoBlock}px`;
-        }
-    }
+    menuBlock('.menu__item_link', '.menu__logo', 'bigLogo', '.menu__wrapper', 'flex-end');
+    pageUp('.pageup');
     videoHeight('.curs__video')
 })
