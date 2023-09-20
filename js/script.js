6b0f0b9fc4c7ff4d034af8fc8ee3bece5d95203d
Andriy Chirka <andrey.chirka@gmail.com>
2023-09-20 18:57:24 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 //jQuery
-$(document).on('ready', function () {
-
+$(document).on("ready", function () {
 	// $('.modal__close').on('click', function () {
 	// 	$('.overlay, #thanks').fadeOut();
 	// });
 
-	document.addEventListener('keydown', (e) => {
-		if (e.code === "Escape" && ($('#thanks').css('display') === 'block')) {
-			$('.overlay, #consultation, #order, #thanks').fadeOut();
-			console.log('1');
+	document.addEventListener("keydown", (e) => {
+		if (e.code === "Escape" && $("#thanks").css("display") === "block") {
+			$(".overlay, #consultation, #order, #thanks").fadeOut();
+			console.log("1");
 		}
 	});
 
@@ -19,11 +18,11 @@ $(document).on('ready', function () {
 				surname: "required",
 				phone: {
 					required: true,
-					minlength: 10
+					minlength: 10,
 				},
 				email: {
 					required: true,
-					email: true
+					email: true,
 				},
 				checkbox: "required",
 			},
@@ -32,71 +31,69 @@ $(document).on('ready', function () {
 				surname: "Введіть Ваше прізвище",
 				phone: {
 					required: "Введіть Ваш номер телефону",
-					minlength: "Зразок: +380ххххххххх (10 цифр)"
+					minlength: "Зразок: +380ххххххххх (10 цифр)",
 				},
 				email: {
 					required: "Введіть Ваш e-mail",
-					email: "Зразок: name@domain.com"
+					email: "Зразок: name@domain.com",
 				},
 				checkbox: "Потрібно погодитись з політикою конфіденційності",
-			}
+			},
 		});
 	}
-	validateForms('#connect-form');
-	$('input[name=phone]').mask("+38 (999) 999-9999").on('click', function () {
-		if ($(this).val() === '+38 (___) ___-____') { //перемещаем принудительно курсор на начало ввода номера
-			$(this).get(0).setSelectionRange(5, 5);
-		}
-	});
+	validateForms("#connect-form");
+	$("input[name=phone]")
+		.mask("+38 (999) 999-9999")
+		.on("click", function () {
+			if ($(this).val() === "+38 (___) ___-____") {
+				//перемещаем принудительно курсор на начало ввода номера
+				$(this).get(0).setSelectionRange(5, 5);
+			}
+		});
 });
 
-
 //js
-window.addEventListener('DOMContentLoaded', () => {
-
-	const preload = document.querySelector('.bckg'),
-		resume = document.querySelector('.resume'),
-		promo = document.querySelector('.promo'),
-		portfolio = document.querySelector('.portfolio'),
-
+window.addEventListener("DOMContentLoaded", () => {
+	const preload = document.querySelector(".bckg"),
+		resume = document.querySelector(".resume"),
+		promo = document.querySelector(".promo"),
+		portfolio = document.querySelector(".portfolio"),
 		user = detect.parse(navigator.userAgent), //подключаемый модуль, для определения браузера устройства
+		menu = document.querySelector(".menu"),
+		overlay = document.querySelectorAll(".portfolio__overlay"),
+		top1 = document.querySelectorAll(".portfolio__descr"),
+		btn = document.querySelectorAll(".portfolio__link"),
+		hamburger = document.querySelector(".hamburger"),
+		arrows = document.querySelector(".promo__arrows"),
+		pageUp = document.querySelector(".pageup");
 
-		menu = document.querySelector('.menu'),
-		overlay = document.querySelectorAll('.portfolio__overlay'),
-		top1 = document.querySelectorAll('.portfolio__descr'),
-		btn = document.querySelectorAll('.portfolio__link'),
-		hamburger = document.querySelector('.hamburger'),
+	const form = document.querySelector("#connect-form");
+	const modalThanksOverlay = document.querySelector(".overlay");
+	const modalThanks = document.querySelector("#thanks");
 
-		arrows = document.querySelector('.promo__arrows'),
-		pageUp = document.querySelector('.pageup');
-
-	const form = document.querySelector('#connect-form');
-	const modalThanksOverlay = document.querySelector('.overlay');
-	const modalThanks = document.querySelector('#thanks');
-
-	form.addEventListener('submit', function (e) {
+	form.addEventListener("submit", function (e) {
 		e.preventDefault();
 
 		if (!this.checkValidity()) {
 			return;
 		}
 
-		fetch('mailer/smart.php', {
-				method: 'POST',
-				body: new FormData(this)
-			})
+		fetch("mailer/smart.php", {
+			method: "POST",
+			body: new FormData(this),
+		})
 			.then(function (response) {
 				if (response.ok) {
-					form.querySelectorAll('input').forEach(input => {
-						input.value = '';
+					form.querySelectorAll("input").forEach((input) => {
+						input.value = "";
 					});
 					fadeIn(modalThanks, 1000);
 					fadeIn(modalThanksOverlay, 1000);
 					form.reset();
 				}
 			})
-			.catch(error => {
-				console.log('Error:', error);
+			.catch((error) => {
+				console.log("Error:", error);
 			});
 
 		return false;
@@ -104,18 +101,18 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	const fadeIn = (el, timeout, opacity, visibility, display) => {
 		el.style.opacity = opacity || 1;
-		el.style.display = display || 'block';
-		el.style.visibility = visibility || 'visible';
+		el.style.display = display || "block";
+		el.style.visibility = visibility || "visible";
 		el.style.transition = `all ${timeout}ms`;
 	};
 
 	const fadeOut = (el, timeout, opacity, visibility) => {
 		el.style.opacity = opacity || 0;
-		el.style.visibility = visibility || 'hidden';
+		el.style.visibility = visibility || "hidden";
 		el.style.transition = `all ${timeout}ms`;
 	};
 
-	window.addEventListener('scroll', () => {
+	window.addEventListener("scroll", () => {
 		if (window.scrollY >= 300) {
 			fadeOut(arrows, 1000);
 		} else {
@@ -128,34 +125,41 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	});
 
-
-	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini|Safari|Chrome Mobile/i.test(user.browser.name)) { //проверка регулярными выражениями на название браузера
+	if (
+		/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini|Safari|Chrome Mobile/i.test(
+			user.browser.name
+		)
+	) {
+		//проверка регулярными выражениями на название браузера
 		promo.style.backgroundAttachment = "scroll";
 		resume.style.backgroundAttachment = "scroll";
 		portfolio.style.backgroundAttachment = "scroll";
-
 	}
 
 	function ready() {
-		document.body.style.overflow = '';
+		document.body.style.overflow = "";
 		preload.remove();
 	}
 
 	window.addEventListener("load", ready);
 
 	function toggleMenuClass(action) {
-		menu.classList[action]('active');
-		hamburger.classList[action]('hide');
+		menu.classList[action]("active");
+		hamburger.classList[action]("hide");
 	}
 
-	document.body.addEventListener('click', (e) => {
-		if (e.target.closest('.hamburger')) {
-			toggleMenuClass('add');
+	document.body.addEventListener("click", (e) => {
+		if (e.target.closest(".hamburger")) {
+			toggleMenuClass("add");
 		}
-		if (e.target.closest('.menu__close') || e.target.closest('.menu__overlay') || e.target.closest('.menu__item')) {
-			toggleMenuClass('remove');
+		if (
+			e.target.closest(".menu__close") ||
+			e.target.closest(".menu__overlay") ||
+			e.target.closest(".menu__item")
+		) {
+			toggleMenuClass("remove");
 		}
-		if (e.target.closest('.modal__close')) {
+		if (e.target.closest(".modal__close")) {
 			fadeOut(modalThanks, 1000, 0);
 			fadeOut(modalThanksOverlay, 1000, 0);
 		}
@@ -164,30 +168,30 @@ window.addEventListener('DOMContentLoaded', () => {
 	function togglePortfolioClass(target, event, action) {
 		target.forEach((item, i) => {
 			item.addEventListener(event, () => {
-				overlay[i].classList[action]('opacity');
-				top1[i].classList[action]('top');
-				btn[i].classList[action]('hide');
+				overlay[i].classList[action]("opacity");
+				top1[i].classList[action]("top");
+				btn[i].classList[action]("hide");
 			});
 		});
 	}
 
-	togglePortfolioClass(overlay, 'mouseover', 'add');
-	togglePortfolioClass(overlay, 'mouseout', 'remove');
-	togglePortfolioClass(btn, 'mouseover', 'add');
-	togglePortfolioClass(btn, 'mouseout', 'remove');
+	togglePortfolioClass(overlay, "mouseover", "add");
+	togglePortfolioClass(overlay, "mouseout", "remove");
+	togglePortfolioClass(btn, "mouseover", "add");
+	togglePortfolioClass(btn, "mouseout", "remove");
 
 	function onDisplay(entry) {
 		entry.forEach(function (change) {
 			if (change.isIntersecting) {
-				change.target.classList.add('Delay');
+				change.target.classList.add("Delay");
 			} else {
-				change.target.classList.remove('Delay');
+				change.target.classList.remove("Delay");
 			}
 		});
 	}
-	const elements = document.querySelectorAll('.animate');
+	const elements = document.querySelectorAll(".animate");
 	const options = {
-		threshold: [0.2]
+		threshold: [0.2],
 	};
 	const observer = new IntersectionObserver(onDisplay, options);
 	for (let elm of elements) {
