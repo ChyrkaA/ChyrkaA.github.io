c29ea8e64539cfe59c50bc53c32ca9e4f05aba70
Andriy Chirka <andrey.chirka@gmail.com>
2024-02-24 20:28:59 +0200

    update

--- ilve/script.js
+++ ilve/script.js
@@ -91,40 +91,89 @@
   !*** ./src/js/main.js ***!
   \************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-function onDisplay(entry) {
-  entry.forEach(function (change) {
-    if (change.isIntersecting) {
-      change.target.classList.add("Delay");
+"use strict";
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  const preload = document.querySelector(".overlay");
+  function ready() {
+    document.querySelector("body").removeAttribute("style");
+    preload.remove();
+  }
+  window.addEventListener("load", ready);
+  function onDisplay(entry) {
+    entry.forEach(function (change) {
+      if (change.isIntersecting) {
+        change.target.classList.add("Delay");
+      } else {
+        change.target.classList.remove("Delay");
+      }
+    });
+  }
+  const elements = document.querySelectorAll(".animate");
+  const options = {
+    threshold: [1]
+  };
+  const observer = new IntersectionObserver(onDisplay, options);
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
+  const menu = document.querySelector(".menu");
+  let lastScrollTop = 0;
+  function showTopMenu() {
+    let scrollTop = window.scrollY || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+      menu.classList.add("hide__menu");
     } else {
-      change.target.classList.remove("Delay");
+      menu.classList.remove("hide__menu");
+    }
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+    if (window.scrollY > 500) {
+      menu.classList.add("white__menu");
+    } else {
+      menu.classList.remove("white__menu");
+    }
+  }
+  window.addEventListener("scroll", showTopMenu);
+  const hamburger = document.querySelector(".hamburger");
+  const menuBig = document.querySelector(".menu__big");
+  const menuList = document.querySelector(".menu__list");
+  hamburger.addEventListener("click", () => {
+    if (menu.classList.contains("menu__height")) {
+      window.addEventListener("scroll", showTopMenu);
+      menuBig.classList.toggle("menu__big_show");
+      menu.classList.toggle("menu__height");
+      menu.classList.toggle("menu__full_white");
+      menuList.classList.toggle("menu__list_hide");
+    } else {
+      removeEventListener("scroll", showTopMenu);
+      menuBig.classList.toggle("menu__big_show");
+      menu.classList.toggle("menu__height");
+      menu.classList.toggle("menu__full_white");
+      menuList.classList.toggle("menu__list_hide");
     }
   });
-}
-const elements = document.querySelectorAll(".animate");
-const options = {
-  threshold: [0.7]
-};
-const observer = new IntersectionObserver(onDisplay, options);
-for (let elm of elements) {
-  observer.observe(elm);
-}
-const menu = document.querySelector(".menu");
-let lastScrollTop = 0;
-window.addEventListener("scroll", () => {
-  let scrollTop = window.scrollY || document.documentElement.scrollTop;
-  if (scrollTop > lastScrollTop) {
-    menu.classList.add("hide__menu");
-  } else {
-    menu.classList.remove("hide__menu");
-  }
-  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
-  if (window.scrollY > 500) {
-    menu.classList.add("white__menu");
-  } else {
-    menu.classList.remove("white__menu");
-  }
+  const arrows = document.querySelector(".promo__arrows");
+  const fadeIn = (el, timeout, opacity, visibility, display) => {
+    el.style.opacity = opacity || 1;
+    el.style.display = display || "block";
+    el.style.visibility = visibility || "visible";
+    el.style.transition = `all ${timeout}ms`;
+  };
+  const fadeOut = (el, timeout, opacity, visibility) => {
+    el.style.opacity = opacity || 0;
+    el.style.visibility = visibility || "hidden";
+    el.style.transition = `all ${timeout}ms`;
+  };
+  window.addEventListener("scroll", () => {
+    if (window.scrollY >= 300) {
+      fadeOut(arrows, 1000);
+    } else {
+      fadeIn(arrows, 1000, 1, "visible", "flex");
+    }
+  });
 });
 
 /***/ })
