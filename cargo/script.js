eeebc485dc1f03a2b68e2b2614a5713b758e5993
Andriy Chirka <andrey.chirka@gmail.com>
2024-03-16 00:09:36 +0200

    update

--- cargo/script.js
+++ cargo/script.js
@@ -100,16 +100,20 @@ window.addEventListener("DOMContentLoaded", () => {
   const overlay = document.querySelector(".overlay");
   const modalBig = document.querySelector(".modal_big");
   const modalMini = document.querySelector(".modal_mini");
-  window.addEventListener("click", e => {
-    if (e.target.classList.contains("button")) {
-      overlay.classList.add("overlay__active");
-      modalBig.classList.add("modal__active");
-    } else if (e.target.classList.contains("modal__close") || e.target.classList.contains("overlay")) {
-      overlay.classList.remove("overlay__active");
-      modalBig.classList.remove("modal__active");
-      modalMini.classList.remove("modal__active");
-    }
-  });
+  function action(action) {
+    window.addEventListener(action, e => {
+      if (e.target.classList.contains("button")) {
+        overlay.classList.add("overlay__active");
+        modalBig.classList.add("modal__active");
+      } else if (e.target.classList.contains("modal__close") || e.target.classList.contains("overlay")) {
+        overlay.classList.remove("overlay__active");
+        modalBig.classList.remove("modal__active");
+        modalMini.classList.remove("modal__active");
+      }
+    });
+  }
+  action("click");
+  action("touchend");
   const forms = document.querySelectorAll("form");
   forms.forEach(form => {
     form.addEventListener("submit", function (e) {
