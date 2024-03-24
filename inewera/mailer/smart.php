5645e8f0d61c50a40c8ab462073babbc81959afa
Andriy Chirka <andrey.chirka@gmail.com>
2024-03-24 19:08:31 +0200

    update

---
+++ inewera/mailer/smart.php
@@ -0,0 +1,46 @@
+<?php
+
+$name = $_POST['name'];
+$phone = $_POST['number'];
+$email = $_POST['email'];
+$text = $_POST['text'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP(); // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers
+$mail->SMTPAuth = true; // Enable SMTP authentication
+$mail->Username = 'pdacademy.info@gmail.com'; // Наш логин
+$mail->Password = 'pdwtjvgzmdqfgzlj'; // Наш пароль от ящика
+$mail->SMTPSecure = 'ssl'; // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to
+
+$mail->setFrom('pdacademy.info@gmail.com', 'PDA'); // От кого письмо 
+$mail->addAddress('pdacademy.info@gmail.com'); // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true); // Set email format to HTML
+
+$mail->Subject = 'Дані';
+$mail->Body = '
+		Користувач лишив такі дані <br> 
+	Ім’я: ' . $name . ' <br>
+	Номер телефону: ' . $phone . '<br>
+	E-mail: ' . $email . '<br>
+	Текст повідомлення: ' . $text . '';
+
+if (!$mail->send()) {
+	return false;
+} else {
+	return true;
+}
+
+?>
--- inewera/script.js
+++ inewera/script.js
@@ -148,6 +148,46 @@ window.addEventListener("DOMContentLoaded", () => {
   for (let elm of elements) {
     observer.observe(elm);
   }
+  window.addEventListener("click", e => {
+    if (e.target.classList.contains("button")) {
+      overlayModal.classList.add("overlay-modal__active");
+      modalBig.classList.add("modal__active");
+    } else if (e.target.classList.contains("modal__close") || e.target.classList.contains("overlay-modal")) {
+      overlayModal.classList.remove("overlay-modal__active");
+      modalBig.classList.remove("modal__active");
+      modalMini.classList.remove("modal__active");
+    }
+  });
+  const overlayModal = document.querySelector(".overlay-modal");
+  const modalBig = document.querySelector(".modal_big");
+  const modalMini = document.querySelector(".modal_mini");
+  const forms = document.querySelectorAll("form");
+  forms.forEach(form => {
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+      fetch("mailer/smart.php", {
+        method: "POST",
+        body: new FormData(this)
+      }).then(function (response) {
+        if (response.ok) {
+          form.querySelectorAll("input").forEach(input => {
+            input.value = "";
+          });
+          modalBig.classList.remove("modal__active");
+          overlayModal.classList.add("overlay-modal__active");
+          modalMini.classList.add("modal__active");
+          setTimeout(function () {
+            overlayModal.classList.remove("overlay__active");
+            modalMini.classList.remove("modal-modal__active");
+          }, 5000);
+          form.reset();
+        }
+      }).catch(error => {
+        console.log("Error:", error);
+      });
+      return false;
+    });
+  });
 });
 
 /***/ })
