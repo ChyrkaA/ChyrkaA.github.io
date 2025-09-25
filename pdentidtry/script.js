4709fb2905fa82b457216372eab52bd9cb31174b
Andriy Chirka <andrey.chirka@gmail.com>
2025-09-25 21:51:05 +0300

    update

--- pdentidtry/script.js
+++ pdentidtry/script.js
@@ -10665,6 +10665,67 @@ document.addEventListener('DOMContentLoaded', () => {
   for (let elm of elements) {
     observer.observe(elm);
   }
+  const write = document.querySelector('.write');
+  const messenger = document.querySelector('.write__btn-messenger');
+  const whatsapp = document.querySelector('.write__btn-whatsapp');
+  const viber = document.querySelector('.write__btn-viber');
+  const telegram = document.querySelector('.write__btn-telegram');
+  const phone = document.querySelector('.write__btn-phone');
+  window.addEventListener('click', e => {
+    if (!e.target.closest('.write')) {
+      messenger.removeAttribute('id');
+      setTimeout(() => {
+        whatsapp.removeAttribute('id');
+      }, 100);
+      setTimeout(() => {
+        viber.removeAttribute('id');
+      }, 200);
+      setTimeout(() => {
+        telegram.removeAttribute('id');
+      }, 300);
+      setTimeout(() => {
+        phone.removeAttribute('id');
+      }, 400);
+    } else {
+      setTimeout(() => {
+        messenger.setAttribute('id', 'messenger');
+      }, 400);
+      setTimeout(() => {
+        whatsapp.setAttribute('id', 'whatsapp');
+      }, 300);
+      setTimeout(() => {
+        viber.setAttribute('id', 'viber');
+      }, 200);
+      setTimeout(() => {
+        telegram.setAttribute('id', 'telegram');
+      }, 100);
+      phone.setAttribute('id', 'phone');
+    }
+  });
+  const overlayModal = document.querySelector('.overlay-modal');
+  const modalBig = document.querySelector('.modal_big');
+  const modalMini = document.querySelector('.modal_mini');
+  const modalDirector = document.querySelector('.modal_director');
+  window.addEventListener('click', e => {
+    if (e.target.classList.contains('menu__center-contact')) {
+      modalOpen(modalBig, 'overlay-modal__active', 'modal__active');
+      console.log(1);
+    } else if (e.target.classList.contains('modal__close') || e.target.classList.contains('overlay-modal')) {
+      modalClose('overlay-modal__active', 'modal__active');
+    } else if (e.target.classList.contains('footer__left-write')) {
+      modalOpen(modalDirector, 'overlay-modal__active', 'modal__active');
+    }
+  });
+  function modalOpen(reciver, overlayModalActive, modalActive) {
+    overlayModal.classList.add(overlayModalActive);
+    reciver.classList.add(modalActive);
+  }
+  function modalClose(overlayModalActive, modalActive) {
+    overlayModal.classList.remove(overlayModalActive);
+    modalBig.classList.remove(modalActive);
+    modalDirector.classList.remove(modalActive);
+    modalMini.classList.remove(modalActive);
+  }
 });
 })();
 
